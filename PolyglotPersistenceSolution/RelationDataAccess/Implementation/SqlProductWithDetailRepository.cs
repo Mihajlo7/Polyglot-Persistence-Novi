c54acd108435f4fbe53d8bfa6e38f2ce1de3d601
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-11-12 14:53:46 +0100

    Have added methods for products with details

--- PolyglotPersistenceSolution/RelationDataAccess/Implementation/SqlProductWithDetailRepository.cs
+++ PolyglotPersistenceSolution/RelationDataAccess/Implementation/SqlProductWithDetailRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
+using IDataAccess;
+using Microsoft.Data.SqlClient;
+using RelationDataAccess.HelperSqlData;
+using RelationDataAccess.Resources;
 
 namespace RelationDataAccess.Implementation
 {
-    public class SqlProductWithDetailRepository 
+    public class SqlProductWithDetailRepository : IProductWithDetailsRepository
     {
         private readonly string _connectionString;
         private readonly string _database;
@@ -15,6 +20,102 @@ namespace RelationDataAccess.Implementation
         {
             _database = database;
             _connectionString = $"Data Source=.;Initial Catalog={database};Integrated Security=True;TrustServerCertificate=True;";
+        }
+
+        public Task<List<ProductModel>> GetProductsWithDetailByProductName(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductModel>> GetProductsWithDetailsByJoin()
+        {
+            string query = ProductWithDetail.GetProductWithDetailsLeftJoin;
+
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(query, connection);
+            command.CommandTimeout = 300;
+
+            connection.Open();
+            using var reader = await command.ExecuteReaderAsync();
+            var products = reader.GetProductWithDetailsBadWay();
+            return products;
+        }
+
+        public async Task<List<ProductModel>> GetProductsWithDetailsByJoinOptimised()
+        {
+            string query = ProductWithDetail.GetProductsWithDetailsByJoinOptimised;
+
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(query, connection);
+            command.CommandTimeout = 300;
+
+            connection.Open();
+            using var reader = await command.ExecuteReaderAsync();
+            var products = reader.GetProductsWithDetails();
+            return products;
+        }
+
+        public async Task<List<ProductModel>> GetProductsWithDetailsBySubQuery()
+        {
+            string query=ProductWithDetail.GetProductsWithDetailsBySubQuery;
+
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(query, connection);
+            command.CommandTimeout = 300;
+
+            connection.Open();
+            using var reader = await command.ExecuteReaderAsync();
+            var products = reader.GetProductsWithDetails();
+            return products;
+        }
+
+        public async Task<List<ProductModel>> GetProductsWithDetailsBySubQueryUsingApply()
+        {
+            string query = ProductWithDetail.GetProductsWithDetailsBySubQueryApply;
+
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(query, connection);
+            command.CommandTimeout = 300;
+
+            connection.Open();
+            using var reader = await command.ExecuteReaderAsync();
+            var products = reader.GetProductsWithDetails();
+            return products;
+        }
+
+        public Task<ProductModel> GetProductWithDetailByProductId(long productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertMany(List<ProductModel> product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertManyBulk(List<ProductModel> products)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertOne(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateLongDescriptionByProductId(long productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePriceByYearManifactured(int yearManifactured)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStorageByProductId(long productId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
